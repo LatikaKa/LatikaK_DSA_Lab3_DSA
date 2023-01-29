2eff87259693b46c7624cd23f7a9d46fa572ba80
LatikaKa <119791765+LatikaKa@users.noreply.github.com>
2023-01-29 13:30:15 +0530

    Add files via upload

---
+++ Lab3_29JAN23/src/Quest2_Pair/Node.java
@@ -0,0 +1,9 @@
+package Quest2_Pair;
+
+public class Node {
+	int data; Node left = null, right = null;
+	Node(int data)
+	{
+		this.data = data;
+	}
+}
---
+++ Lab3_29JAN23/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Latika kadam desai
+ *
+ */
+module Lab3_29JAN23 {
+}
